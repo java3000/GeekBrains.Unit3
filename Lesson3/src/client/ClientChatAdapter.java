61d94c1327c3906435c03279e23009acfd11ec68
Aleksey <lexa709@mail.ru>
2021-01-04 13:14:19 +0300

    lesson3-base

---
+++ Lesson3/src/client/ClientChatAdapter.java
@@ -0,0 +1,33 @@
+package client;
+
+
+import gui.ChatFrame;
+
+public class ClientChatAdapter {
+    private ChatFrame chatFrame;
+    private Client client;
+
+    public ClientChatAdapter(String host, int port) {
+        client = new Client(host, port);
+        chatFrame = new ChatFrame(messageFromFormSubmitListener -> client.sendMessage(messageFromFormSubmitListener));
+        read();
+    }
+
+    private void read() {
+        new Thread(() -> {
+            try {
+                while (true) {
+                    chatFrame.append(
+                            client.receiveMessage()
+                    );
+                }
+            } catch (ClientConnectionException e) {
+                throw e;
+            } finally {
+                if (client != null) {
+                    client.close();
+                }
+            }
+        }).start();
+    }
+}
---
+++ Lesson3/src/server/AuthenticationService.java
@@ -0,0 +1,56 @@
+package server;
+
+//import com.mysql.jdbc.Driver
+
+import java.util.Set;
+
+public class AuthenticationService {
+    private Set<CredentialsEntry> entries;
+
+
+    public AuthenticationService() {
+        update();
+    }
+
+    public void update() {
+        entries = DataBaseHelper.getUsers();
+    }
+
+    public String findNicknameByLoginAndPassword(String login, String password) {
+        for (CredentialsEntry entry : entries) {
+            if (entry.getLogin().equals(login) && entry.getPassword().equals(password)) {
+                return entry.getNickname();
+            }
+        }
+        return null;
+    }
+
+    public static class CredentialsEntry {
+        private String login;
+        private String password;
+        private String nickname;
+
+        public CredentialsEntry(String login, String password, String nickname) {
+            this.login = login;
+            this.password = password;
+            this.nickname = nickname;
+        }
+
+
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+    }
+}
