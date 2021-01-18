1d15ad38367555cf40c9d33c94878c8fb24c1f50
Aleksey <lexa709@mail.ru>
2021-01-18 17:55:20 +0300

    lesson4

---
+++ Lesson4/src/src/client/ClientChatAdapter.java
@@ -0,0 +1,37 @@
+package src.client;
+
+import src.gui.ChatFrame;
+import src.helpers.FileHelper;
+
+public class ClientChatAdapter {
+    private ChatFrame chatFrame;
+    private Client client;
+
+    public ClientChatAdapter(String host, int port) {
+        client = new Client(host, port);
+        chatFrame = new ChatFrame(messageFromFormSubmitListener -> client.sendMessage(messageFromFormSubmitListener));
+        //2
+        chatFrame.append(FileHelper.readHistory(100));
+        read();
+    }
+
+    private void read() {
+        new Thread(() -> {
+            try {
+
+                while (true) {
+                    String message = client.receiveMessage();
+                    chatFrame.append(message);
+                    //1
+                    FileHelper.writeHistory(message);
+                }
+            } catch (ClientConnectionException e) {
+                throw e;
+            } finally {
+                if (client != null) {
+                    client.close();
+                }
+            }
+        }).start();
+    }
+}
